5219a6a0489bea2a24f3874a9d07a036f23fcc0d
Sway Xu <youyoue666@gmail.com>
2013-07-05 17:02:06 +0000

    modified web.js for reading index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
